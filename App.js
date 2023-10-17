bda3ae3ccc2562367e24c7e9937cb2935e27275a
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2023-10-17 09:24:13 +0000

    Add boilerplate code

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeUI from "./src/components/HomeUI";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      // This will load the HomeUI page
+      <HomeUI/>
     </View>
   );
 }
