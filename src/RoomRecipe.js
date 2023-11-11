b55a6067aaa0f0788785642f426835fc34c078dc
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2023-11-11 21:44:34 +0000

    almost functionable heart feature

--- src/RoomRecipe.js
+++ src/RoomRecipe.js
@@ -12,7 +12,6 @@ const RoomRecipe = () => {
       style={styles.container}
       contentContainerStyle={{ paddingBottom: 100 }}
     >
-
       <YoutubePlayer
         height={200}
         play={true}
