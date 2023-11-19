02348e1298bd41ba301cda5895e4f870d4aefdd6
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2023-11-19 14:09:59 +0000

    copy favorites to favorite screen, partially

--- src/FavoriteScreen.js
+++ src/FavoriteScreen.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, FlatList, StyleSheet, useWindowDimensions, Dimensions } from 'react-native';
+import { loadFaveState } from "./AsyncStorage";
+import { DATA } from "./data";
+import Cards from "./Cards";
 
 const FavoriteScreen = () => {
+  const { height, width } = useWindowDimensions();
+  const [isPortrait, setIsPortrait] = useState(height > width);
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      const { height, width } = Dimensions.get("window");
+      setIsPortrait(height > width);
+    };
+
+    Dimensions.addEventListener("change", handleOrientationChange);
+  }, []);
+
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    const loadFavorites = async () => {
+      const favoritePromises = DATA.map(async (recipe) => {
+        const storedFavState = await loadFaveState(recipe.id);
+        if (storedFavState) {
+          return recipe;
+        }
+        return null;
+      });
+
+      Promise.all(favoritePromises).then((favorites) => {
+        const filteredFavorites = favorites.filter((recipe) => recipe !== null);
+        setFavoriteRecipes(filteredFavorites);
+        console.log("loading favorite state");
+      });
+    };
+
+    loadFavorites();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Favorite Screen</Text>
-      {/* Add your favorite content here */}
+      <FlatList
+        style={isPortrait ? styles.portraitCard : styles.landscapeCard}
+        data={favoriteRecipes}
+        keyExtractor={(item) => item.id}
+        horizontal={!isPortrait}
+        renderItem={({ item }) => <Cards recipe={item} />}
+      />
     </View>
   );
 };
@@ -16,9 +58,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  portraitCard: {
+    // Your styles for portrait mode
+  },
+  landscapeCard: {
+    // Your styles for landscape mode
   },
 });
 
