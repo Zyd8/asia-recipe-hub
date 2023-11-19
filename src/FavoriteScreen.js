b2e5a64231770b4474f0cd32bcd95ccddb0dfd7c
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2023-11-19 15:27:32 +0000

    add darkmode support and message when favorite tab is empty

--- src/FavoriteScreen.js
+++ src/FavoriteScreen.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { View, FlatList, StyleSheet, useWindowDimensions, Dimensions } from 'react-native';
+import { Text, View, FlatList, StyleSheet, useWindowDimensions, Dimensions } from 'react-native';
 import { loadFaveState } from "./AsyncStorage";
 import { DATA } from "./data";
 import Cards from "./Cards";
+import { useFocusEffect } from '@react-navigation/native';
+import { loadDarkModeState } from "./AsyncStorage";
 
-const FavoriteScreen = () => {
+const FavoriteScreen = ({ navigation }) => {
   const { height, width } = useWindowDimensions();
   const [isPortrait, setIsPortrait] = useState(height > width);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
 
   useEffect(() => {
     const handleOrientationChange = () => {
@@ -17,57 +20,93 @@ const FavoriteScreen = () => {
     Dimensions.addEventListener("change", handleOrientationChange);
   }, []);
 
-  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const loadFavorites = async () => {
+    const favoritePromises = DATA.map(async (recipe) => {
+      const storedFavState = await loadFaveState(recipe.id);
+      if (storedFavState) {
+        return recipe;
+      }
+      return null;
+    });
+
+    Promise.all(favoritePromises).then((favorites) => {
+      const filteredFavorites = favorites.filter((recipe) => recipe !== null);
+      setFavoriteRecipes(filteredFavorites);
+      console.log("loading favorite state");
+    });
+  };
 
   useEffect(() => {
-    const loadFavorites = async () => {
-      const favoritePromises = DATA.map(async (recipe) => {
-        const storedFavState = await loadFaveState(recipe.id);
-        if (storedFavState) {
-          return recipe;
-        }
-        return null;
-      });
-
-      Promise.all(favoritePromises).then((favorites) => {
-        const filteredFavorites = favorites.filter((recipe) => recipe !== null);
-        setFavoriteRecipes(filteredFavorites);
-        console.log("loading favorite state");
-      });
-    };
-
     loadFavorites();
-  }, []);
+  }, [isPortrait]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [navigation, isPortrait]) 
+  );
+
+  const [darkmode, setDarkMode] = useState(false);
+
+  const loadDarkMode = async () => {
+    const storedDarkMode = await loadDarkModeState();
+    setDarkMode(storedDarkMode);
+    console.log("loading darkmode state");
+  };
+  
+  useFocusEffect(
+    React.useCallback(() => {
+      loadDarkMode();
+    }, [navigation, isPortrait]) 
+  );
+
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        style={isPortrait ? styles.portraitCard : styles.landscapeCard}
-        data={favoriteRecipes}
-        keyExtractor={(item) => item.id}
-        horizontal={!isPortrait}
-        renderItem={({ item }) => <Cards recipe={item} />}
-      />
+    <View style={darkmode ? styles.darkContainer : styles.lightContainer}>
+      {favoriteRecipes.length === 0 ? (
+        <Text style={darkmode ? styles.darkText : styles.lightText}>You don't have any favorite recipes yet.</Text>
+      ) : (
+        <FlatList
+          style={isPortrait ? styles.portraitCard : styles.landscapeCard}
+          data={favoriteRecipes}
+          keyExtractor={(item) => item.id}
+          horizontal={!isPortrait}
+          renderItem={({ item }) => <Cards recipe={item} />}
+        />
+      )}
     </View>
   );
 };
 
+
 const styles = StyleSheet.create({
-  container: {
+  lightContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: "#FCF9F2",
+  },
+  darkContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#323233",
   },
   portraitCard: {
     marginTop: 50,
     width: "100%"
   },
   landscapeCard: {
-    //height: "100%",
-    margin: 90,
-    marginBottom: 100,
-    height: "300"
-    
+    height: "100%",
+    marginTop: 100,
+  },
+  lightText: {
+    fontSize: 18,
+    color: "#AB8476",
+  },
+  darkText: {
+    fontSize: 18,
+    color: "white",
   },
 });
 
