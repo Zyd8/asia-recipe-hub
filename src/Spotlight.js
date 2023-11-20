f0a37d45d698ebe075e7622d2118c5e168aa1138
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2023-11-20 17:23:55 +0000

    Carousel is clickable

--- src/Spotlight.js
+++ src/Spotlight.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Image, StyleSheet, useWindowDimensions, Dimensions } from "react-native";
+import { View, Image, StyleSheet, useWindowDimensions, Dimensions, TouchableOpacity } from "react-native";
 import Carousel from "react-native-snap-carousel";
+import { useNavigation } from '@react-navigation/native'; 
 import { DATA } from "./data";
 
 const Spotlight = () => {
+  const navigation = useNavigation();
   const { height, width } = useWindowDimensions();
   const [isPortrait, setIsPortrait] = useState(height > width);
 
@@ -17,15 +19,28 @@ const Spotlight = () => {
 
   }, []);
 
+  const handleCarouselItemClick = (recipeId) => {
+    const selectedRecipe = DATA.find(recipe => recipe.id === recipeId);
+  
+    if (selectedRecipe) {
+      console.log(`Clicked on Recipe ID: ${recipeId}`);
+      navigation.navigate('Recipe', { recipe: selectedRecipe });
+    } else {
+      console.warn(`Recipe with ID ${recipeId} not found.`);
+    }
+  };
+
   return (
     <View style={styles.carouselContainer}>
       {isPortrait && (
         <Carousel
           data={DATA}
-          renderItem={({ item }) => (
-            <View style={styles.slide}>
-              <Image source={item.image} style={styles.image} />
-            </View>
+          renderItem={({ item, index }) => (
+            <TouchableOpacity onPress={() => handleCarouselItemClick(item.id)}>
+              <View style={styles.slide}>
+                <Image source={item.image} style={styles.image} />
+              </View>
+            </TouchableOpacity>
           )}
           sliderWidth={width}
           itemWidth={250}
